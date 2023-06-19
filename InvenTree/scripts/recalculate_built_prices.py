204242a6fc1ac1d4f9501004b175a3aa9c3544e9
InvenTree server <inventree@co2.home.mattb.net.nz>
2023-06-20 01:13:30 +1200

    Refactor recalc logic to match a74546d

--- InvenTree/scripts/recalculate_built_prices.py
+++ InvenTree/scripts/recalculate_built_prices.py
@@ -3,8 +3,6 @@
 # hence the need to recalculate...
 #
 # run as python manage.py shell < scripts/recalculate_built_prices.py
-from django.db.models import Avg
-
 from InvenTree.status_codes import StockHistoryCode, BuildStatus
 from build.models import Build
 from stock.models import StockItemTracking
@@ -17,31 +15,50 @@ for b in builds:
   # Build lookup of bom quantities
   q = {}
   for i in b.untracked_bom_items:
-    q[i.sub_part.pk] = i.quantity
+    q[i.sub_part.pk] = (i.sub_part.pk, int(i.quantity))
     # including descendants, in case the BoM item is a template/variant part
     for i2 in i.sub_part.get_descendants(include_self=False):
-      q[i2.pk] = i.quantity
+      q[i2.pk] = (i.sub_part.pk, int(i.quantity))
 
   # Find untracked stock items which contributed to this build
-  base_price = 0.0
+  si = {}
   qs = StockItemTracking.objects.filter(tracking_type=StockHistoryCode.BUILD_CONSUMED, deltas__buildorder=b.pk)
-  for i in qs.values('item__part__pk', 'item__part__name').annotate(avg_price=Avg('item__purchase_price')):
-    price =  i['avg_price'] * q[i['item__part__pk']]
-    print(f' + {price:.4f} for {i["item__part__name"]} x{q[i["item__part__pk"]]} ({i["avg_price"]:.4f}/each (avg))')
-    base_price += float(price)
-  print(f' = Sub-total for untracked parts is ${base_price:.4f}')
+  for i in qs.values('item__part__pk', 'item__part__name', 'item__purchase_price', 'deltas__quantity'):
+    bi, biq = q[i['item__part__pk']]
+    pp = i['item__purchase_price']
+    iq = float(i['deltas__quantity'])
+    print(f' * {iq:.0f}x {i["item__part__name"]} (for {bi}) @ {pp:.4f}')
+    si.setdefault(bi, [])
+    for _ in range(0,int(iq)):
+      si[bi].append(float(pp))
+  print()
 
   # Now iterate the outputs and look for tracked stock items
-  for o in b.build_outputs.all():
+  for n, o in enumerate(b.build_outputs.all()):
     if o.is_building:
       continue
-    print(f' * Output #{o.serial} from {o.batch} has original price of {o.purchase_price}')
-    price = base_price
+    orig = 0.0
+    if o.purchase_price:
+      orig = round(float(o.purchase_price.amount),4)
+    print(f' * Output #{o.serial} from {o.batch} has original price of {orig}')
+    price = 0.0
+    pd = []
+    for bi, pl in si.items():
+      _, biq = q[bi]
+      partprice = 0.0
+      for nn in range(0,biq):
+        pli = (biq * n) + nn
+        tprice = pl[pli]
+        pd.append(f'{bi}@{tprice:.4f}')
+        partprice += tprice
+      price += partprice
+    pdstr = ', '.join(pd)
+    print(f'  + NZ${price:.2f} for untracked parts ({pdstr})')
     for i in o.installed_parts.all():
       print(f'  + {i.purchase_price} for {i.part.name} with serial #{i.serial}')
       price += float(i.purchase_price.amount)
     print(f'  = ${price:.4f} expected purchase_price')
-    if round(price, 4) != round(float(o.purchase_price.amount),4):
+    if round(price, 4) != orig:
       o.purchase_price = price
       o.save()
       print(f'  ! FIXED!')
